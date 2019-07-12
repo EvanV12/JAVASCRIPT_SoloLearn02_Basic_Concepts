25ea6186e7b514d0f14ad49dfb744e507297dd6f
EvanV12 <44060244+EvanV12@users.noreply.github.com>
2019-07-12 16:35:22 +0300

    Added "Data Types"

---
+++ JAVASCRIPT_SoloLearn02_Basic_Concepts/WebContent/b_Data_Types.js
@@ -0,0 +1,18 @@
+var num = 42;// A number without decimals
+
+// Floating-Point Numbers
+var price = 55.5;
+document.write(price);
+
+// Strings
+var name = 'John';
+var text = "My name is John Smith";
+var text2 = "My name is 'John' ";
+
+// backslash (\) escape character
+var sayHello = 'Hello world! \'I am a JavaScript programmer.\'';
+document.write(sayHello);
+
+// Booleans
+var isActive = true;
+var isHoliday = false;
