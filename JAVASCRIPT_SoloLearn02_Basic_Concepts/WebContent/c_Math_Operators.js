4d3bdf1d1f3f7e40ab6fab37c0c00e9727e48875
EvanV12 <44060244+EvanV12@users.noreply.github.com>
2019-07-13 04:20:49 +0300

    Added "Math Operators"

---
+++ JAVASCRIPT_SoloLearn02_Basic_Concepts/WebContent/c_Math_Operators.js
@@ -0,0 +1,20 @@
+// Arithmetic Operators
+var x = 10 + 5;
+document.write(x);	// Outputs 15
+
+var x2 = 10;
+var y = x2 + 5 + 22 + 45 + 6548;
+document.write(y);	// Outputs 6630
+
+
+// Multiplication
+var x3 = 10 * 5;
+document.write(x3);	// Outputs 50
+
+// Division
+var x4 = 100 / 5;
+document.write(x4);	// Outputs 20
+
+// The Modulus
+var myVariable = 26 % 6;	// myVariable equals 2
+
