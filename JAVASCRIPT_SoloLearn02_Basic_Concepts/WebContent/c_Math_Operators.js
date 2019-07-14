d275cc085f8c7eb0eb9a048946f0e57c5704c875
EvanV12 <44060244+EvanV12@users.noreply.github.com>
2019-07-14 17:41:56 +0300

    Added "Increment & Decrement"

--- JAVASCRIPT_SoloLearn02_Basic_Concepts/WebContent/c_Math_Operators.js
+++ JAVASCRIPT_SoloLearn02_Basic_Concepts/WebContent/c_Math_Operators.js
@@ -18,3 +18,24 @@ document.write(x4);	// Outputs 20
 // The Modulus
 var myVariable = 26 % 6;	// myVariable equals 2
 
+
+// Increment & Decrement
+// var++ Post Increment
+	var a1 = 0, b1 = 10;
+	var a1 = b1++;
+	// a = 10 and b = 11;
+
+// ++var Pre Increment
+	var a2 = 0, b2 = 10;
+	var a2 = ++b2;
+	// a = 11 and b = 11
+
+// var-- Post Decrement
+	var a3 = 0, b3 = 10;
+	var a3 = b3--;
+	// a = 10 and b = 9
+	
+// --var Pre Decrement
+	var a4 = 0, b4 = 10;
+	var a4 = --b4;
+	// a = 9 and b = 9
