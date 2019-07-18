4bcde97e19ef188637ca930c8bd3db357fb25b0c
EvanV12 <44060244+EvanV12@users.noreply.github.com>
2019-07-18 18:31:27 +0300

    Added "Comparison Operators"

---
+++ JAVASCRIPT_SoloLearn02_Basic_Concepts/WebContent/e_Comparison_Operators.js
@@ -0,0 +1,24 @@
+// equal to (==)
+var num = 10;
+// num == 8 will return false
+
+
+/*
+
+==		Equal to									5 == 10		false
+
+=== 	Identical (equal and of the same type)		5 === 10	false
+
+!=		Not equal to								5 != 10		true
+
+!==		Not Identical								10 !== 10	false
+
+>		Greater than								10 > 5		true
+
+>=		Greater than or equal to					10 >= 5		true
+
+<		Less than									10 < 5		false
+
+<=		Less than or equal to						10 <= 5		false
+
+*/
