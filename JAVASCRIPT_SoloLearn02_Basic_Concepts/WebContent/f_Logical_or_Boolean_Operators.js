cf3ad895d42159608ed5559cc4ae7aca323521b7
EvanV12 <44060244+EvanV12@users.noreply.github.com>
2019-07-19 07:09:03 +0300

    Added "Logical or Boolean Operators"

---
+++ JAVASCRIPT_SoloLearn02_Basic_Concepts/WebContent/f_Logical_or_Boolean_Operators.js
@@ -0,0 +1,13 @@
+// Logical Operators
+// && (AND)		Returns true, if both operands are true
+// || (OR)		Returns true, if one of the operands is true
+// ! (NOT)		Returns true, if the operand is false, and false, if the operand is true
+
+
+(4 > 2) && (10 < 15)	// Returns true
+
+
+// Conditional (Ternary) Operator
+// variable = (condition) ? value1: value2
+
+var isAdult = (age < 18) ? "Too Young": "Old enough";
